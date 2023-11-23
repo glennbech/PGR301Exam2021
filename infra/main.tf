cccefd33a8a09c26c00c22f3a34adbe918be90d1
Martin <ec2-user@ip-172-31-36-90.eu-west-1.compute.internal>
2023-11-23 12:56:05 +0000

    backend and provider setup for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "pgr301-2021-terraform-state"
+    key            = "kand2021/state/apprunner.state"
+    region         = "eu-north-1"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
 variable "service_name" {
   description = "The name of the AWS App Runner service"
   type        = string
